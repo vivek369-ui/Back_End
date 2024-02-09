4e535fc760154f1f8665888970b66eab6c64413d
Vivek Gite <118560898+vivek369-ui@users.noreply.github.com>
2024-02-09 12:59:04 +0530

    Update String_Buffer.java

--- workspace1/basic_part_one/src/try_error_base/String_Buffer.java
+++ workspace1/basic_part_one/src/try_error_base/String_Buffer.java
@@ -6,10 +6,13 @@ public class String_Buffer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		StringBuffer str = new StringBuffer("vivek");
+		StringBuffer str_1 = new StringBuffer("vivek");
+		System.out.println(str.equals(str_1));
 		System.out.println(""+str);
 		System.out.println(str.append(" Gite "));
 		System.out.println(str.insert(5, " Balabhau "));
 	    System.out.println(str.delete(5, 14));
+		
 	    System.out.println(str.reverse());
 	    System.out.println(str.length());
 	    System.out.println(str.indexOf("vivek"));
