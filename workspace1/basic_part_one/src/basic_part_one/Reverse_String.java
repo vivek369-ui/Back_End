bd7508b613da32735cc7372a0e201a515568558d
Vivek Gite <118560898+vivek369-ui@users.noreply.github.com>
2024-02-16 12:27:49 +0530

    Update Reverse_String.java

--- workspace1/basic_part_one/src/basic_part_one/Reverse_String.java
+++ workspace1/basic_part_one/src/basic_part_one/Reverse_String.java
@@ -6,15 +6,24 @@ public class Reverse_String {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//
-//		Scanner Sc = new Scanner(System.in);
-//		System.out.println("input string:");
-//		char letters[] = Sc.nextLine().toCharArray();
-//		System.out.println("revese the String:");
-//		for (int i = letters.length - 1; i >= 0; i--) {
-//			System.out.print(letters[i]);
-//		}
-//		System.out.println("\n");
+// String str = "Hello";
+// 		char[] charArray = str.toCharArray();
+
+// 		// Reverse the char array using swapping
+// 		int start = 0;
+// 		int end = charArray.length - 1;
+// 		while (start < end) {
+
+// 			char temp = charArray[start];
+// 			charArray[start] = charArray[end];
+// 			charArray[end] = temp;
+// 			start++;
+// 			end--;
+// 		}
+
+// 		        // Convert char array back to string
+// 		        String reversedStr = new String(charArray);
+// 		        System.out.println(reversedStr); 
 		 String st ="ram";
 		 String rev ="";
 		 System.out.println(""+st);
